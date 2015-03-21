7aa82098030863b17aae1f3750107be761c0da0c
owq <owq@gmx.com>
2015-03-21 23:54:21 +0800

    select2

---
+++ userscripts/select2.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        Select2
+// @namespace   owq
+// @description Replace unwieldy dropdowns
+// @include        *sgx.com*
+// @require http://cdn.jsdelivr.net/jquery/2.1.1/jquery.min.js
+// @require  http://cdn.jsdelivr.net/chosen/1.1.0/chosen.jquery.min.js
+// @downloadURL https://owq.github.io/userscripts/select2.user.js
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+
+$(function() {
+  
+  $("head").append('<link rel="stylesheet" href="http://cdn.jsdelivr.net/chosen/1.1.0/chosen.css">');
+  
+  setTimeout(function() {
+    //console.log($("qsShow").value);
+    $("select").each(function() {
+      var curr = this;
+      var chosen = $(curr).chosen();
+    });
+  }, 500);
+  
+  console.log("choseN");
+});
