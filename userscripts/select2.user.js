06d7da55729c21f7122a46b9b611472a77c6deb0
owq <owq@gmx.com>
2016-07-24 18:44:21 +0800

    Replace select with datalist instead for native performance

--- userscripts/select2.user.js
+++ userscripts/select2.user.js
@@ -1,42 +1,27 @@
 // ==UserScript==
 // @name        Select2
 // @namespace   owq
-// @description Replace unwieldy dropdowns
+// @description Replace unwieldy dropdowns with datalist
 // @include        *sgx.com*
-// ==require http://cdn.jsdelivr.net/jquery/2.1.1/jquery.min.js==
-// @require  https://cdnjs.cloudflare.com/ajax/libs/chosen/1.4.2/chosen.jquery.min.js
 // @downloadURL https://owq.github.io/userscripts/select2.user.js
-// @version     1.2
+// @version     1.3
 // @grant       none
 // ==/UserScript==
 
-function chosen(t) {
-  t.chosen({
-    search_contains: true
-  })
-}
 $(function () {
-  $('head').append('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.4.2/chosen.css">');
-  $('head').append('<link rel="stylesheet" href="https://owq.github.io/userscripts/chosen-firefox-mod.css">');
-  
-  window.addEventListener('load', function () {
-    // your code here
-    var selects = $('select');
-    
-    selects.each(function () {
-      var $this = $(this);
-      $this.on('chosen:hiding_dropdown', function() {
-         $(this).chosen('destroy');
-      });
-      $this.on('mousedown',  function(e) {
-        e.preventDefault();
-        this.blur();
-        window.focus();
-        var xthis = $(this);
-        chosen(xthis);
-        xthis.trigger("chosen:open");
-      })
-    });
-    
-  }, false);
+    var oldPopCombo = populateCombo;
+    populateCombo = function(cid, jName, aName, item) {
+        if(item.items.length < 30) {
+            oldPopCombo(cid, jName, aName, item);
+            return;
+        }
+
+        var el = $(document.getElementById(cid));
+        var id = el[0].id;
+        var name = el[0].name;
+        var dlId = id + "dataList";
+        el.before("<input list='"+dlId+"' id='"+id+"' name='"+name+"'>");
+        el.replaceWith("<datalist id='"+dlId+"'></datalist>");
+        oldPopCombo(dlId, jName, aName, item);
+    };
 });
