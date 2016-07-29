29b1121439cbe40d08a309163df608fc6337c2c1
owq <owq@gmx.com>
2016-07-29 20:45:37 +0800

    Make it work for firefox by running script at document-start

--- userscripts/select2.user.js
+++ userscripts/select2.user.js
@@ -4,24 +4,34 @@
 // @description Replace unwieldy dropdowns with datalist
 // @include        *sgx.com*
 // @downloadURL https://owq.github.io/userscripts/select2.user.js
-// @version     1.3
+// @version     1.4
 // @grant       none
+// @run-at document-start
 // ==/UserScript==
 
-$(function () {
-    var oldPopCombo = populateCombo;
-    populateCombo = function(cid, jName, aName, item) {
-        if(item.items.length < 30) {
-            oldPopCombo(cid, jName, aName, item);
-            return;
-        }
+var oldPopCombo;
+myPopulateCombo = function(cid, jName, aName, item) {
+    if(item.items.length < 30) {
+        oldPopCombo(cid, jName, aName, item);
+        return;
+    }
 
-        var el = $(document.getElementById(cid));
-        var id = el[0].id;
-        var name = el[0].name;
-        var dlId = id + "dataList";
-        el.before("<input list='"+dlId+"' id='"+id+"' name='"+name+"'>");
-        el.replaceWith("<datalist id='"+dlId+"'></datalist>");
-        oldPopCombo(dlId, jName, aName, item);
-    };
+    var el = $(document.getElementById(cid));
+    var id = el[0].id;
+    var name = el[0].name;
+    var dlId = id + "dataList";
+    var input = $("<input list='"+dlId+"' id='"+id+"' name='"+name+"'>");
+    el.before(input);
+    el.replaceWith("<datalist id='"+dlId+"'></datalist>");
+    oldPopCombo(dlId, jName, aName, item);
+    
+    input.keyup(function (e) {
+        if (e.which == 13) { // Enter key pressed
+            fetchResult();
+        }
+    });
+};
+Object.defineProperty(window, 'populateCombo', {
+    get: function() { return myPopulateCombo; },
+    set: function(fn) { oldPopCombo = fn; }
 });
