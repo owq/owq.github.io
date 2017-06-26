2c8df7f29b283743c202ff70ec409287ebe9f061
owq <owq@gmx.com>
2017-06-26 12:06:05 +0800

    Support new changes (modern look with image thumbnail)

--- userscripts/thefinance.user.js
+++ userscripts/thefinance.user.js
@@ -2,7 +2,7 @@
 // @name        thefinance skipper
 // @namespace   owq
 // @include     *thefinance.sg*
-// @version     1.3
+// @version     1.4
 // @downloadURL https://owq.github.io/userscripts/thefinance.user.js
 // @grant       none
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js
@@ -15,29 +15,39 @@ $(function() {
 
     $.each(articles, function(k, article) {
         var art = $(article);
-        var link = art.find(".more-link")[0];
+        var link = art.find("a.post-thumbnail, a.featured-image");
         var titleLink = art.find(".entry-title a")[0];
 
-        var oldTitle = titleLink.innerHTML;
-        titleLink.innerHTML += "~";
+        function myOnClick() {
+            var oldTitle = titleLink.innerHTML;
+            titleLink.innerHTML += "~";
+            //link.prop("innerHTML", "~");
+            
+            var temp = $("<div></div>");
+            temp.load(titleLink.href + " .post-wrap", function(){
+                var finalLink = temp.find("a").last();
+                var finalLinkHref = finalLink.attr("href");
+                //console.log(finalLink);
+                if(!finalLinkHref) return;
 
-        //console.log(link);
-        link.innerHTML += "~";
-        var temp = $("<div></div>");
-        temp.load(link.href + " .entry-content", function(){
-            var finalLink = temp.children().first().children("a").last();
-            var finalLinkHref = finalLink.attr("href");
-            //console.log(finalLink);
-            if(!finalLinkHref) return;
+                link.prop('href', finalLinkHref);
+                //link.prop('innerHTML', "Go to article");
 
-            link.href = finalLinkHref;
-            link.innerHTML = "Go to article";
+                titleLink.href = finalLinkHref;
 
-            titleLink.href = link.href;
-            titleLink.innerHTML = oldTitle;
-
-
-        });
+                location.href = finalLinkHref;
+                
+                titleLink.innerHTML = oldTitle;
+            });
+        }
+        
+        function goLink(e) {
+            e.preventDefault();
+            myOnClick();
+        }
+        
+        link.click(goLink);
+        $(titleLink).click(goLink);
     });
 
 });
