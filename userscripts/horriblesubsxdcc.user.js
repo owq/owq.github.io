95a2b54f841655a81a3e261fd8859acd090f0e53
owq <owq@gmx.com>
2017-08-24 18:36:18 +0800

    Update horriblesubsxdcc.user.js
    use correct selector

--- userscripts/horriblesubsxdcc.user.js
+++ userscripts/horriblesubsxdcc.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Horriblesubs XDCC link adder
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        http://horriblesubs.info/
@@ -12,16 +12,14 @@
     'use strict';
     var $ = jQuery;
     $(function() {
-        $(document).click(".rls-label", function(e) {
+        $(document).on("click", ".rls-label", function(e) {
             var els = $(e.target).contents();
             els.splice(0,1);
             var name = els.text();
-            
+
             var url = "http://xdcc.horriblesubs.info/?search=" + name + " [720p]";
             window.open(url, "_blank");
-            
+
         });
     });
-
-    // Your code here...
 })();
