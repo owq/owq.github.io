2190460eb821d5a0f1643bd956e920a8fa0dc78c
owq <owq@gmx.com>
2017-10-04 19:18:57 +0800

    hs: account for unlinked releases

--- userscripts/horriblesubsxdcc.user.js
+++ userscripts/horriblesubsxdcc.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Horriblesubs XDCC link adder
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        http://horriblesubs.info/
@@ -14,8 +14,15 @@
     $(function() {
         $(document).on("click", ".rls-label", function(e) {
             var els = $(e.target).contents();
-            els.splice(0,1);
-            var name = els.text();
+            var name;
+            if(els.length > 1) {
+                els.splice(0,1);
+                name = els.text();
+            } else {
+                var arr = els.text().split(" ");
+                arr.splice(0,1);
+                name = arr.join(" ");
+            }
 
             var url = "http://xdcc.horriblesubs.info/?search=" + name + " [720p]";
             window.open(url, "_blank");
